249b69c4573899ad3532a8f2d81ac396304480a3
JuDePom <jimmy.etienne@aliceadsl.fr>
2016-04-26 20:03:49 +0200

    perm.v
    Il manque le 3ème subgoal (tauto) dans perm_incl

--- perm.v
+++ perm.v
@@ -27,112 +27,149 @@ Section list_perm.
 
   where "x '~p' y " := (perm x y).
 
-  Fact perm_refl l : l ~p l. (* Cours *)
+  Fact perm_refl l : l ~p l.
   Proof.
-   induction l as [
-                  |list head IH]. (*Division en deux subgoals*)
-   apply perm_nil. (*Afin d'enlever le premier subgoal*)
-   apply perm_cons. (* Afin d'enlever le second subgoal*)
-   apply IH. (*Appliquer IH pour enlever le dernier subgoal*)
+    induction l as [
+                   | l x IH
+                   ].
+
+    (* subgoal 1 *)
+    apply perm_nil.
+
+    (* subgoal 2 *)
+    apply perm_cons.
+    apply IH.
   Qed.
+
 
   Fact perm_length l1 l2 : l1 ~p l2 -> length l1 = length l2.
   Proof.
-    intros H. (*Introduire hypothèse qui dit que l1 permute avec l2*)
+    intros H.
     induction H as [ 
-                   | x l1 l2 H1 IH1 (*Si on ajoute x à l1 et à l2 et que length l1 = length l2 
-                   alors elles font toujours la même taille*)
-                   | x y l (*Ajouter deux éléments pas dans le même ordre 
-                   pour montrer que l'ordre n'influe pas*)
+                   | x l1 l2 H1 IH1
+                   | x y l
                    | l1 l2 l3 H1 IH1 H2 IH2 
                    ].
-    apply refl_equal. (* Reflexivity :S'applique sur le but length nil = length nil*)
-    simpl. (* Permet d'enlever les x:: et de travailler directement
-    sur les listes *)
-    f_equal. (* Permet d'éliminer la fonction S des deux côtés*)
-    apply IH1. (* Appliquer IH1 *)
-    simpl. (* Enleve les x et y des sous buts *) 
-    apply refl_equal. (* Reflexivity *)
-    transitivity (length l2). (* Transitivité sur l2 *)
+
+    (* subgoal 1 *)
+    reflexivity.
+
+    (* subgoal 2 *)
+    simpl.
+    f_equal.
+    apply IH1.
+
+    (* subgoal 3 *)
+    simpl.
+    reflexivity.
+
+    (* subgoal 4 *)
+    transitivity (length l2).
     apply IH1.
     apply IH2.
-   Qed.
+  Qed.
+
 
   Fact perm_sym l1 l2 : l1 ~p l2 -> l2 ~p l1.
   Proof.
-   intros H.
-   induction H. (* Création des 4 cas possibles *)
-   apply perm_nil.
-   apply perm_cons.
-   assumption. (* Applique IHperm *)
-   apply perm_swap.
-   apply perm_trans with l2. (* Pour pouvoir utiliser IHperm1 et IHperm2 *) 
-   apply IHperm2.
-   apply IHperm1.
+    intros H.
+    induction H.
+
+    (* subgoal 1 *)
+    apply perm_nil.
+
+    (* subgoal 2 *)
+    apply perm_cons.
+    apply IHperm.
+
+    (* subgoal 3 *)
+    apply perm_swap.
+
+    (* subgoal 4 *)
+    apply perm_trans with l2. 
+    apply IHperm2.
+    apply IHperm1.
   Qed.
+
 
   Fact perm_middle x l r : x::l++r ~p l++x::r.
   Proof.
-    induction l as [ | y list IHl ]. (* 1 sous but avec le cas de la liste vide et
-    un sous but avec le swap *)
-    simpl. (* Enlever les nil *)
-    apply perm_refl. (* Permet d'enlever un sous but *)
-    simpl. (*Permet d'enlever les parenthèses inutiles *)
+    induction l as [
+                   | y list IHl
+                   ].
+
+    (* subgoal 1 *)
+    simpl.
+
+    (* subgoal 2 *)
+    apply perm_refl.
+    simpl.
     apply perm_trans with (1 := perm_swap _ _ _).
     apply perm_cons.
     apply IHl.
   Qed.
 
+
   Let perm_app_left l r1 r2 : r1 ~p r2 -> l++r1 ~p l++r2.
   Proof.
     intros H.
-    induction l. (*Sous but avec le cas de la liste vide 
-    et un sous but avec le swap*)
-    simpl. (* Enlever les nil *)
+    induction l.
+
+    (* subgoal 1 *)
+    simpl.
     apply H.
-    simpl. (* Enlever parenthèses*)
+
+    (* subgoal 2 *)
+    simpl.
     apply perm_cons.
     apply IHl.
   Qed.
 
+
   Fact perm_app l1 l2 r1 r2 : l1 ~p l2 -> r1 ~p r2 -> l1++r1 ~p l2++r2.
   Proof.
    intros H.
-   revert H r1 r2. (* Rentre les hypothèses dans le subgoal *)
+   revert H r1 r2.
    intros H.
    induction H as [ 
-                   | x l1 l2 H1 IH1 
-                   | x y l 
-                   | l1 l2 l3 H1 IH1 H2 IH2 
-                   ].
-    simpl . (* Enlever les nil *)
-    intros H3 H4 H5.
-    apply H5.  (*Enlever le premier subgoal*)
-    intros H3 H4 H5.
+                  | x l1 l2 H1 IH1 
+                  | x y l 
+                  | l1 l2 l3 H1 IH1 H2 IH2 
+                  ].
+
+    (*subgoal 1 *)
     simpl.
-    apply perm_cons. (*Permet d'enlever les x*)
+    intros ? ? H5.
+    apply H5.
+    
+    (*subgoal 2 *)
+    intros ? ? ?.
+    simpl.
+    apply perm_cons.
     apply IH1.
-    apply H5. (* Enlever le second subgoal *)
-    intros H3 H4 H5.
+    apply H.
+
+    (*subgoal 3 *)
+    intros ? ? ?.
     simpl.
-    apply perm_trans with (1 := perm_swap _ _ _). (*Permet de rendre le memvre de gauche 
-    identique à celui de droite*)
-    apply perm_cons. (*enlève y*)  
-    apply perm_cons. (*Enlève x*)
+    apply perm_trans with (1 := perm_swap _ _ _).
+    apply perm_cons.  
+    apply perm_cons.
     apply perm_app_left.
-    apply H5. (* Enlève le troisième sous but *)
+    apply H.
+
     (*subgoal 4 *)
-    intros H3 H4 H5.
-    apply perm_trans with (l2 ++ H4). (* Faire le lien entre l1 et l3 *)
+    intros ? ? ?.
+    apply perm_trans with (l2 ++ r2).
     apply IH1.
-    apply H5.
-    apply perm_trans with (l3++H3).
+    apply H.
+    apply perm_trans with (l3++r1).
     apply IH2.
-    apply perm_sym. (* Pour appliquer H5 *)
-    apply H5.
+    apply perm_sym.
+    apply H.
     apply perm_app_left.
-    apply H5. (* Résolution du dernier sous but *)
-   Qed.
+    apply H.
+  Qed.
 
 
   (* incl est défini dans la librairie standard, fichier List.v *)
@@ -146,13 +183,31 @@ Section list_perm.
                    | x l1 l2 H1 IH1 
                    | x y l 
                    | l1 l2 l3 H1 IH1 H2 IH2 
-                   ].    
-    intros H1 H2.
-    apply H2. (* Résolution du premier sous-but *)        
-    apply incl_cons.
-    apply in_cons. (* Enlève le x :: !!! *)
+                   ]. 
+    (* subgoal 1 *)    
+    intros ? ?.
+    apply H.
     
-  Admitted.
+    (* subgoal 2 *)
+    intros ? ?.
+    destruct H.
+    left.
+    apply H.
+    right.
+    apply IH1.
+    apply H.
+    
+    (* subgoal 3 *)
+    intros ? ?.
+    revert H.
+    simpl.
+    tauto. (** ! TODO **)
+
+    (* subgoal 4 *)
+    revert IH2.
+    revert IH1.
+    apply incl_tran.
+  Qed.
 
 End list_perm.
 
